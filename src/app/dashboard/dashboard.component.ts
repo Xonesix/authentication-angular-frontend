3de6c45045b5973d3c321114b61b8b1586d5582a
Xonesix <arjuntx@gmail.com>
2024-09-29 16:52:43 -0500

    Finished auth

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  imports: [RouterOutlet],
+  template: `
+    <h1>This is the dashboard page</h1>
+    <ul>
+      <li>Username: {{ email }}</li>
+      <li>Info: "Aramse"</li>
+    </ul>
+  `,
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
+@Injectable({ providedIn: 'root' })
+export class DashboardComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) {}
+  ngOnInit(): void {
+    this.loadData();
+  }
 
+  email = '';
+
+  loadData(): void {
+    // Function to fetch data based on the valid session
+    console.log('Fetching user data...');
+    // Make HTTP requests to fetch necessary data
+    this.http
+      .get<unknown>('http://localhost:8080/dashboard', {
+        withCredentials: true,
+      })
+      .subscribe(
+        (response: any) => {
+          this.email = response.message;
+        },
+        (error) => {
+          this.router.navigate(['/']);
+          // console.error(error);
+          // this.email = 'Something has gone wrong';
+        }
+      );
+  }
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
-
+import { Injectable } from '@angular/core';
 import { Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-form',
   standalone: true,
   imports: [ReactiveFormsModule, RouterOutlet],
   template: `
     <form [formGroup]="profileForm" id="userform" (ngSubmit)="handleSubmit()">
-      <label for="username">UserName: </label>
-      <input required type="text" id="username" formControlName="username" />
+      <label for="email">Email: </label>
+      <input required type="text" id="email" formControlName="email" />
       <label for="password">Password: </label>
       <input
         required
@@ -19,30 +20,49 @@ import { Validators } from '@angular/forms';
         formControlName="password"
       />
       <button type="submit" [disabled]="!profileForm.valid">Submit</button>
+      <p>{{ errorMessage }}</p>
     </form>
   `,
   styleUrl: './form.component.css',
 })
+@Injectable({ providedIn: 'root' })
 export class FormComponent {
   profileForm = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.maxLength(10),
-      Validators.minLength(3),
-    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [
       Validators.required,
       Validators.maxLength(25),
       Validators.minLength(7),
     ]),
   });
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) {}
+  errorMessage = '';
   handleSubmit() {
     // alert(
     //   `Username = ${this.profileForm.value.username}\nPassword = ${this.profileForm.value.password}`
     // );
-    if (true) {
-      this.router.navigate(['dashboard']);
-    }
+
+    const formData = this.profileForm.value;
+    this.http
+      .post<unknown>('http://localhost:8080/login', formData, {
+        observe: 'response',
+        withCredentials: true,
+      })
+      .subscribe(
+        (response: any) => {
+          this.router.navigate(['dashboard']);
+        },
+        (error) => {
+          this.errorMessage = error.error?.message;
+        }
+      );
+
+    // if (true) {
+    //   this.router.navigate(['dashboard']);
+    // }
   }
 }
